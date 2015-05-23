6cba0f99d0d8a9c41eb6e538be167f6efb99d490
AKotiranta <ajkotira@cs.helsinki.fi>
2015-05-23 16:30:51 +0300

    Viikko 2

--- config/routes.php
+++ config/routes.php
@@ -7,3 +7,16 @@
   $routes->get('/hiekkalaatikko', function() {
     HelloWorldController::sandbox();
   });
+
+  $routes->get('/suunnitelmat', function() {
+    HelloWorldController::suunnitelmat_index();
+  });
+  $routes->get('/suunnitelmat/list', function() {
+    HelloWorldController::chore_list();
+  });
+  $routes->get('/suunnitelmat/list/edit', function() {
+    HelloWorldController::chore_edit();
+  });
+
+
+  
